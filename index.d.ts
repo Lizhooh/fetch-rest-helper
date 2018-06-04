6d79e6249e88615d18e8a7ec532423f6d61b7222
lizhooh <960316408@qq.com>
2018-06-04 16:52:01 +0800

    update: index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,70 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 
-export interface OptionsA {
+export interface Options {
     host: string,
     json?: Boolean,
     headers?: any,
 }
 
-export interface OptionsB {
-    host?: string,
-    json?: Boolean,
-    headers?: any,
-    body?: Object | any,
-    query?: Object,
-}
-
 export default class Helper {
-    constructor(prefix: string, options: OptionsA);
+    constructor(prefix: string, options: Options);
 
     /** GET /users */
-    index: (opt: OptionsB) => Promise<any>;
+    index: (opt: {
+        host?: string,
+        json?: Boolean,
+        headers?: any,
+        body?: Object | any,
+        query?: Object,
+    }) => Promise<any>;
     /** GET /users/:id */
-    show: (id: string | number, opt: OptionsB) => Promise<any>;
+    show: (id: string | number, opt: {
+        host?: string,
+        json?: Boolean,
+        headers?: any,
+        body?: Object | any,
+        query?: Object,
+    }) => Promise<any>;
     /** POST /users */
-    create: (opt: OptionsB) => Promise<any>;
+    create: (opt: {
+        host?: string,
+        json?: Boolean,
+        headers?: any,
+        body?: Object | any,
+        query?: Object,
+    }) => Promise<any>;
     /** PUT /users/:id */
-    update: (id: string | number, opt: OptionsB) => Promise<any>;
+    update: (id: string | number, opt: {
+        host?: string,
+        json?: Boolean,
+        headers?: any,
+        body?: Object | any,
+        query?: Object,
+    }) => Promise<any>;
     /** DELETE /users/:id */
-    remove: (id: string | number, opt: OptionsB) => Promise<any>;
+    remove: (id: string | number, opt: {
+        host?: string,
+        json?: Boolean,
+        headers?: any,
+        body?: Object | any,
+        query?: Object,
+    }) => Promise<any>;
     /** GET /users/edit/:id */
-    edit: (id: string | number, opt: OptionsB) => Promise<any>;
+    edit: (id: string | number, opt: {
+        host?: string,
+        json?: Boolean,
+        headers?: any,
+        body?: Object | any,
+        query?: Object,
+    }) => Promise<any>;
     /** GET /users/new */
-    new: (opt: OptionsB) => Promise<any>;
+    new: (opt: {
+        host?: string,
+        json?: Boolean,
+        headers?: any,
+        body?: Object | any,
+        query?: Object,
+    }) => Promise<any>;
 }
 
